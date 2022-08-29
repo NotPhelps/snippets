82d6edfb745396ec72623929421a7c591c69680a
Phelps <94879374+NotPhelps@users.noreply.github.com>
2022-08-28 21:59:07 -0400

    Add files via upload

---
+++ qb-apartments/config.lua
@@ -0,0 +1,18 @@
+Apartments = {}
+
+Apartments.Starting = true
+
+Apartments.SpawnOffset = 30
+
+
+
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "Alta Street Apartments",
+        type = "standardmotel_shell",
+        coords = {
+            enter = vector4(-270.99, -957.92, 31.22, 125.24),
+        }
+    },
+}
